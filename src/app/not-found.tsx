c1430cda7ec8bb8b0729196db2378d12f55611f7
KallMeMoh <kallmemoh@gmail.com>
2026-08-07 18:30:04 +0300

    Add 404 page.

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <div className="max-w-137.5 w-full text-center p-8">
+        <h1 className="text-2xl font-bold mb-4">Page Not Found</h1>
+        <p>
+          It seems like the page you&apos;re looking for is playing hide and
+          seek, just like our professionalism! We&apos;ll keep searching
+          together.
+        </p>
+      </div>
+    </div>
+  );
+}
