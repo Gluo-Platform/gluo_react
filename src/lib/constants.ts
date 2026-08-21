8fac837278d10c7e0b4c1b7723117f0814a022c6
KallMeMoh <kallmemoh@gmail.com>
2026-08-21 14:20:00 +0300

    Move constants and env

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,16 +1,9 @@
 import 'server-only';
 
-import { getEnvVariable } from './utils/getEnvVariable';
-import { ApiResult, ApiResultKind } from './types/apiFetch';
+import { getEnvVariable } from './getEnvVariable';
 
 export const nodeEnv = process.env.NODE_ENV ?? 'production';
 export const apiBaseUrl = getEnvVariable('API_BASE_URL');
 export const cdnBaseUrl = getEnvVariable('NEXT_PUBLIC_CDN_BASE_URL');
 export const backendToken = getEnvVariable('BACKEND_TOKEN');
 export const hCaptchaSecretKey = getEnvVariable('HCAPTCHA_SECRET_KEY');
-
-export const GENERIC_ERROR: ApiResult<never> = {
-  ok: false,
-  type: ApiResultKind.ServerError,
-  message: 'Internal server error',
-};
