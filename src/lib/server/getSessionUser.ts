386f47919ab0ebaf5ca1fe1af525520850e3033d
KallMeMoh <kallmemoh@gmail.com>
2026-08-22 05:35:53 +0300

    Fix importing server only module into a client component bug

--- src/lib/server/getSessionUser.ts
+++ src/lib/server/getSessionUser.ts
@@ -3,26 +3,7 @@ import { cookies } from 'next/headers';
 import { cache } from 'react';
 import { apiFetch } from '../apiFetch';
 import { apiBaseUrl } from '../constants';
-
-// might move it under lib/types
-export type User = {
-  id: string;
-  username: string;
-  avatar: string | null;
-  permissions: number;
-  status: string;
-  banner: {
-    type: string;
-    value: string;
-  };
-  about: string;
-  private: boolean;
-  creation_timestamp: number;
-  streak: number;
-  email_address: string;
-  invisible: boolean;
-  feeds: { id: string; name: string; type: string; icon: string }[];
-};
+import { User } from '../types/user';
 
 export const getSessionUser = cache(async (): Promise<User | null> => {
   const cookieStore = await cookies();
